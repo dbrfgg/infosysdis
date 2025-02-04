3afae862a1b5283260d1703a940bb50faa74109c
dbrfgg <korolevavika805@gmail.com>
2025-02-04 22:34:27 +0300

    lab 1.2 Класс сущности поставщик + инкапсуляция полей

---
+++ Supplier.py
@@ -0,0 +1,7 @@
+class Supplier:
+    def __init__(self, supplier_id, name, address, phone, ogrn):
+        self.__supplier_id = supplier_id
+        self.__name = name
+        self.__address = address
+        self.__phone = phone
+        self.__ogrn = ogrn
