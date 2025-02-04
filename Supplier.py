35aa2a147371fec5ea355fb9d3db6294ef4606d1
dbrfgg <korolevavika805@gmail.com>
2025-02-05 01:04:10 +0300

    lab 1.3 get,set Построить полный класс сущности. Обеспечить инкапсуляцию ВСЕХ полей.

--- Supplier.py
+++ Supplier.py
@@ -5,3 +5,25 @@ class Supplier:
         self.__address = address
         self.__phone = phone
         self.__ogrn = ogrn
+
+# Getters
+    def get_supplier_id(self):
+        return self.__supplier_id
+    def get_name(self):
+        return self.__name
+    def get_address(self):
+        return self.__address
+    def get_phone(self):
+        return self.__phone
+    def get_ogrn(self):
+        return self.__ogrn
+    
+    # Setters
+    def set_name(self, name):
+        self.__name =name
+    def set_address(self, address):
+        self.__address = address
+    def set_phone(self, phone):
+        self.__phone = phone
+    def set_ogrn(self, ogrn):
+        self.__ogrn = ogrn
