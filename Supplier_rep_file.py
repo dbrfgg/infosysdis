62c83ce6223149f929b4bc66adc5efdf777df573
dbrfgg <korolevavika805@gmail.com>
2025-02-15 01:53:40 +0300

    Lab. 2.6 Update class SupplierRepFile Изменение структуры класса, для использования адаптера

--- Supplier_rep_file.py
+++ Supplier_rep_file.py
@@ -4,16 +4,22 @@ from Supplier_rep_File_Strategy import SupplierRepFileStrategy
 class SupplierRepFile:
     
     def __init__(self, strategy: SupplierRepFileStrategy):
+        self._data = []
         self.strategy = strategy
+
+    def write_data_to_file(self):
+        self._strategy.write(self._data)
+
+    def read_data_from_file(self):
+        self._data = self._strategy.read()
     
     def get_all(self):
         """Получить все элементы"""
         return self.strategy.read()
     
     def get_by_id(self, supplier_id):
-        data = self.strategy.read()
-        for entry in data:
-            if entry['supplier_id'] == supplier_id:
+        for entry in self._data:
+            if entry['id'] == id:
                 return entry
         return None
     
@@ -21,17 +27,16 @@ class SupplierRepFile:
         data = self.strategy.read()
         start = (n - 1) * k
         end = start + k
-        return data[start:end]
+        return self._data[start:end]
     
     def sort_by_field(self, field):
         data = self.strategy.read()
         if field in ["name", "address", "phone", "ogrn"]:
-            data.sort(key=lambda x: x.get(field))
-        return data
+            self._data.sort(key=lambda x: x.get(field))
+        return self._data
           
     def add_entity(self, name, address, phone, ogrn):
-        data = self.strategy.read()
-        new_id = max([entry['supplier_id'] for entry in data], default=0) + 1
+        new_id = max([entry['supplier_id'] for entry in self._data], default=0) + 1
         new_entity = {
             'supplier_id': new_id,
             'name': name,
@@ -39,17 +44,15 @@ class SupplierRepFile:
             'phone': phone,
             'ogrn': ogrn
         }
-        if any(entry['ogrn'] == ogrn for entry in data):
+        if any(entry['ogrn'] == ogrn for entry in self._data):
             raise ValueError('огрн должен быть уникальным!')
-        data.append(new_entity)
-        self.strategy.write(data)
+        self._data.append(new_entity)
 
     def replace_by_id(self, supplier_id, name, address, phone, ogrn):
-        data = self.strategy.read()
         entity = self.get_by_id(supplier_id)
         if not entity:
             raise ValueError(f"Элемент с ID {supplier_id} не найден.")
-        if ogrn and ogrn != entity['ogrn'] and any(entry['ogrn'] == ogrn for entry in data):
+        if ogrn and ogrn != entity['ogrn'] and any(entry['ogrn'] == ogrn for entry in self._data):
             raise ValueError('огрн должен быть уникальным!')
         if name:
             entity['name'] = name
@@ -59,16 +62,12 @@ class SupplierRepFile:
             entity['ogrn'] = ogrn
         if phone:
             entity['phone'] = phone
-            self.strategy.write(data)
     
     def delete_by_id(self, supplier_id):
-            data = self.read()
             entity = self.get_by_id(supplier_id)
             if not entity:
                 raise ValueError(f"Элемент с ID {supplier_id} не найден.")
-            data.remove(entity)
-            self.strategy.write(data)
+            self._data.remove(entity)
     
     def get_count(self):
-            data = self.strategy.read()
-            return len(data)
+            return len(self._data)
